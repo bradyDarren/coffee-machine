f5fcb351f9c110c6c54128d5afb6466fbefa8aeb
bradyDarren <dcgbfinancial@gmail.com>
2024-10-21 21:52:41 -0500

    addition of resources and recipes

---
+++ data.py
@@ -0,0 +1,25 @@
+# what the machine will start prior to taking the 1st order.
+resources = { 
+    "water": 1000,
+    "coffee": 1000,
+    "milk": 400
+}
+
+#type of coffee and the ingredients needed to make it.
+recipes = {
+    "expresso":{
+        "water": 30, 
+        "coffee": 18
+    }, 
+    "cappuccino":{
+        "water":30,
+        "coffee": 18,
+        "milk": 120
+    }, 
+    "latte":{
+        "water":30,
+        "coffee":18,
+        "milk":150
+
+    }
+}
