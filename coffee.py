ee7556bf505a28595fead4047acf40905d5d4a4e
bradyDarren <dcgbfinancial@gmail.com>
2024-10-23 07:11:09 -0500

    completion of coin_calc function

--- coffee.py
+++ coffee.py
@@ -1,8 +1,20 @@
 from data import recipes, money, resources
 from art import logo
 
-def coin_input(qcount, dcount, ncount, pcount):
+def coin_calc(qcount, dcount, ncount, pcount):
     qsum = qcount * money["quarter"]
-    print(qsum)
+    dsum = dcount * money["dime"]
+    nsum = ncount * money["nickle"]
+    psum = pcount * money["penny"]
+    total = psum + nsum + dsum + qsum
+    return total
 
-coin_input()
+print(coin_calc(5,3, 5,1))
+
+def report(): 
+
+
+def process_order(coin_total, coffee_selection):
+    
+
+
